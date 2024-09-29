05fd0fb2e4052ed99e7707c968ce7537f4e4f231
Lazar Konstantinou <Lazar.Konstantinou@hhu.de>
2024-09-29 14:24:30 +0200

    no diff

--- src/test/java/de/lordlazor/bachelorarbeit/BachelorarbeitApplicationTests.java
+++ src/test/java/de/lordlazor/bachelorarbeit/BachelorarbeitApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BachelorarbeitApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
