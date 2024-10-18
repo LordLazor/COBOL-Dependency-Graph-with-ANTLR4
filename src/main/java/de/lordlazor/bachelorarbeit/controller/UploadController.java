dd6a19ec99d16e5ee3de72304cbd7f625a1f35bb
Lazar Konstantinou <Lazar.Konstantinou@hhu.de>
2024-10-18 17:25:20 +0200

    UploadController refactoring

--- src/main/java/de/lordlazor/bachelorarbeit/controller/UploadController.java
+++ src/main/java/de/lordlazor/bachelorarbeit/controller/UploadController.java
@@ -36,7 +36,7 @@ public class UploadController {
   }
 
   @PostMapping("/upload/folder")
-  public String uploadFolder(@RequestParam("folder")  MultipartFile[] files, @RequestParam("textInput") String textInput, RedirectAttributes redirectAttributes) {
+  public String uploadFolder(@RequestParam("folder") MultipartFile[] files, @RequestParam("textInput") String textInput, RedirectAttributes redirectAttributes) {
     redirectAttributes.addFlashAttribute("type", "folder");
     if (files.length == 0) {
       redirectAttributes.addFlashAttribute("message", "Please select a folder to upload");
@@ -54,87 +54,14 @@ public class UploadController {
         }
 
         if (!file.getOriginalFilename().endsWith(".cbl") && !file.getOriginalFilename().endsWith(".cob")) {
-          continue; // TODO: Implement copy book support => rn grammar is problematic so that the copy book cant be parsed (prob. wont be able in the future)
+          continue;
         }
 
         InputStream inputStream = file.getInputStream();
         streams.add(CharStreams.fromStream(inputStream));
-
       }
 
-      JsonUtilities jsonUtilities = new JsonUtilities();
-
-      Cobol85Lexer lexer;
-      CommonTokenStream tokens;
-      Cobol85Parser parser;
-
-      ParseTree tree;
-
-      Visitor visitor;
-
-      for (CharStream stream : streams) {
-        lexer = new Cobol85Lexer(stream);
-        tokens = new CommonTokenStream(lexer);
-        parser = new Cobol85Parser(tokens);
-
-        tree = parser.startRule();
-
-        visitor = new Visitor(jsonUtilities);
-        visitor.visit(tree);
-      }
-
-      if (textInput.isBlank()){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy_HHmmss");
-
-        String formattedDateTime = LocalDateTime.now().format(formatter);
-
-        jsonUtilities.createJsonFile(controllerUtilities.getOutputFolder() + formattedDateTime + ".json");
-
-        Files.createDirectories(Paths.get(controllerUtilities.getOutputFolder() + formattedDateTime));
-
-        BufferedReader reader;
-
-        for (MultipartFile file : files) {
-          if (file.getOriginalFilename().endsWith(".cob") || file.getOriginalFilename().endsWith(".cbl")) {
-            reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
-            String extractedFilename = controllerUtilities.findProgramID(reader);
-
-            if (extractedFilename == null) {
-              redirectAttributes.addFlashAttribute("message", "PROGRAM-ID not found in the file");
-              return "redirect:/upload/folder";
-            }
-
-            Files.copy(file.getInputStream(), Paths.get(controllerUtilities.getOutputFolder() + formattedDateTime + "/" + extractedFilename));
-          }
-        }
-
-        redirectAttributes.addFlashAttribute("message", "You successfully uploaded the folder '" + folderName + "'");
-
-      } else {
-
-        jsonUtilities.createJsonFile(controllerUtilities.getOutputFolder() + textInput + ".json");
-        Files.createDirectories(Paths.get(controllerUtilities.getOutputFolder() + textInput));
-
-        BufferedReader reader;
-
-        for (MultipartFile file : files) {
-          if (file.getOriginalFilename().endsWith(".cob") || file.getOriginalFilename().endsWith(".cbl")) {
-            reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
-            String extractedFilename = controllerUtilities.findProgramID(reader);
-
-            if (extractedFilename == null) {
-              redirectAttributes.addFlashAttribute("message", "PROGRAM-ID not found in the file");
-              return "redirect:/upload/folder";
-            }
-
-            Files.copy(file.getInputStream(), Paths.get(controllerUtilities.getOutputFolder() + textInput + "/" + extractedFilename));
-          }
-        }
-
-        redirectAttributes.addFlashAttribute("message", "You successfully uploaded the folder '" + textInput + "'");
-
-      }
-
+      processFiles(streams, textInput, files, redirectAttributes, folderName, "folder");
 
     } catch (IOException e) {
       e.printStackTrace();
@@ -154,65 +81,12 @@ public class UploadController {
     }
 
     try {
-
       InputStream inputStream = file.getInputStream();
-
       CharStream stream = CharStreams.fromStream(inputStream);
+      List<CharStream> streams = new ArrayList<>();
+      streams.add(stream);
 
-      Cobol85Lexer lexer = new Cobol85Lexer(stream);
-      CommonTokenStream tokens = new CommonTokenStream(lexer);
-      Cobol85Parser parser = new Cobol85Parser(tokens);
-
-      ParseTree tree = parser.startRule();
-
-      JsonUtilities jsonUtilities = new JsonUtilities();
-
-      Visitor visitor = new Visitor(jsonUtilities);
-      visitor.visit(tree);
-
-      if (textInput.isBlank()){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy_HHmmss");
-
-        String formattedDateTime = LocalDateTime.now().format(formatter);
-
-        jsonUtilities.createJsonFile(controllerUtilities.getOutputFolder() + formattedDateTime + ".json");
-
-        Files.createDirectories(Paths.get(controllerUtilities.getOutputFolder() + formattedDateTime));
-
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
-        String extractedFilename = controllerUtilities.findProgramID(reader);
-
-        if (extractedFilename == null) {
-          redirectAttributes.addFlashAttribute("message", "PROGRAM-ID not found in the file");
-          return "redirect:/upload/file";
-        }
-
-
-        Files.copy(file.getInputStream(), Paths.get(controllerUtilities.getOutputFolder() + formattedDateTime + "/" +extractedFilename));
-
-
-        redirectAttributes.addFlashAttribute("message", "You successfully uploaded '" + file.getOriginalFilename() + "'");
-      } else {
-
-        jsonUtilities.createJsonFile("src/main/resources/out/" + textInput + ".json");
-        Files.createDirectories(Paths.get(controllerUtilities.getOutputFolder() + textInput));
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
-        String extractedFilename = controllerUtilities.findProgramID(reader);
-
-        if (extractedFilename == null) {
-          redirectAttributes.addFlashAttribute("message", "PROGRAM-ID not found in the file");
-          return "redirect:/upload/file";
-        }
-
-        Files.copy(file.getInputStream(), Paths.get(controllerUtilities.getOutputFolder()+ textInput + "/" + extractedFilename));
-
-
-        redirectAttributes.addFlashAttribute("message", "You successfully uploaded '" + textInput + "'");
-
-      }
-
+      processFiles(streams, textInput, new MultipartFile[]{file}, redirectAttributes, file.getOriginalFilename(), "file");
 
     } catch (IOException e) {
       e.printStackTrace();
@@ -223,4 +97,42 @@ public class UploadController {
     return "redirect:/upload/file";
   }
 
+  private void processFiles(List<CharStream> streams, String textInput, MultipartFile[] files, RedirectAttributes redirectAttributes, String folderName, String type) throws IOException {
+    JsonUtilities jsonUtilities = new JsonUtilities();
+    Cobol85Lexer lexer;
+    CommonTokenStream tokens;
+    Cobol85Parser parser;
+    ParseTree tree;
+    Visitor visitor;
+
+    for (CharStream stream : streams) {
+      lexer = new Cobol85Lexer(stream);
+      tokens = new CommonTokenStream(lexer);
+      parser = new Cobol85Parser(tokens);
+      tree = parser.startRule();
+      visitor = new Visitor(jsonUtilities);
+      visitor.visit(tree);
+    }
+
+    String outputFolder = controllerUtilities.getOutputFolder();
+    String formattedDateTime = textInput.isBlank() ? LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyyy_HHmmss")) : textInput;
+    jsonUtilities.createJsonFile(outputFolder + formattedDateTime + ".json");
+    Files.createDirectories(Paths.get(outputFolder + formattedDateTime));
+
+    for (MultipartFile file : files) {
+      if (file.getOriginalFilename().endsWith(".cob") || file.getOriginalFilename().endsWith(".cbl")) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+        String extractedFilename = controllerUtilities.findProgramID(reader);
+
+        if (extractedFilename == null) {
+          redirectAttributes.addFlashAttribute("message", "PROGRAM-ID not found in the file");
+          return;
+        }
+
+        Files.copy(file.getInputStream(), Paths.get(outputFolder + formattedDateTime + "/" + extractedFilename));
+      }
+    }
+
+    redirectAttributes.addFlashAttribute("message", "You successfully uploaded the " + type + " '" + folderName + "'");
+  }
 }
